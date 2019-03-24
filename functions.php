d4cf7be212aced462c0f2a5c08d55269b03465fa
Rachel McCart <rlmccart@ufl.edu>
2019-03-24 08:02:20 -0500

    update functions

--- functions.php
+++ functions.php
@@ -118,45 +118,14 @@ register_sidebar( array(
 
 //Single Page Widgets
 register_sidebar( array(
-    'name'          => ('About Us Text Block'),
-    'id'            => 'about-us',
+    'name'          => ('About Us Single Text Block'),
+    'id'            => 'about-us-single',
     'description'   => 'Widget area for about us section',
-    'before_widget' => '<div class="about-us">',
+    'before_widget' => '<div class="about-us-single">',
     'after_widget'  => '</div>',
     'before_title'  => '<h3 class="widget-title">',
     'after_title'   => '</h3>'
     ));
-
-register_sidebar( array(
-    'name'          => ('Left Footer'),
-    'id'            => 'left-footer',
-    'description'   => 'Left widget area in the footer',
-    'before_widget' => '<div class="widget-footer widget-left">',
-    'after_widget'  => '</div>',
-    'before_title'  => '<h3 class="widget-title">',
-    'after_title'   => '</h3>'
-    ));
-
-register_sidebar( array(
-    'name'          => ('Middle Footer'),
-    'id'            => 'middle-footer',
-    'description'   => 'Middle widget area in the footer',
-    'before_widget' => '<div class="widget-footer widget-middle">',
-    'after_widget'  => '</div>',
-    'before_title'  => '<h3 class="widget-title">',
-    'after_title'   => '</h3>'
-    ));
-
-register_sidebar( array(
-    'name'          => ('Right Footer'),
-    'id'            => 'right-footer',
-    'description'   => 'Right widget area in the footer',
-    'before_widget' => '<div class="widget-footer widget-right">',
-    'after_widget'  => '</div>',
-    'before_title'  => '<h3 class="widget-title">',
-    'after_title'   => '</h3>'
-    ));
-}
 
 
 add_action('widgets_init', 'blank_widgets_init');
