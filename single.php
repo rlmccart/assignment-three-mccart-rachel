e6b702e8fa1549e96f908a740972b82c70785d29
Rachel McCart <rlmccart@ufl.edu>
2019-03-24 10:18:07 -0500

    single blog post

--- single.php
+++ single.php
@@ -1,24 +1,17 @@
 <? php /*Template Name: Single Blog Post */ get_header(); ?>
 <div class="container">
 
-<<?php
-    if(have_posts()){
-        while(have_posts()){
-            the_post();?>
-            <h2 class="post-title"><?php the_title(); ?></h2>
-            <p class="post-date"><?php echo "Published: " . get_the_date(); ?></p>
-            <div class="post-feature"><?php the_post_thumbnail('thumb'); ?></div>
-            <?php the_content(); ?>
-            <p><?php echo "This post written by: " . get_the_author(); ?></p>
-  <?php }// end while loop
-    } // end if statement
-    ?>
-
+  <!-- Hero Image Section -->
+      <div class="row">
+          <div class="twelve columns">
+              <?php dynamic_sidebar('hero-image-blog'); ?>
+          </div>
+      </div>
 
 <!-- About Us Text Block Section -->
     <div class="row">
         <div class="two-thirds column single">
-            <?php dynamic_sidebar('about-us'); ?>
+            <?php dynamic_sidebar('about-us-single'); ?>
         </div>
         <div class="one-third column sidebar single">
           <!-- sidebar -->
