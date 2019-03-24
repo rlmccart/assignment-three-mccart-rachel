0a7e4b8d20b2a84e3ad4150d851d3fc9413c5e1d
Rachel McCart <rlmccart@ufl.edu>
2019-03-24 10:24:03 -0500

    update page template

--- page.php
+++ page.php
@@ -1,24 +1,22 @@
-<?php get_header(); ?>
-  <div class="container">
+<? php /*Template Name: Single Blog Post */ get_header(); ?>
+<div class="container">
+
+  <!-- Hero Image Section -->
       <div class="row">
-        <div class="nine columns">
-          <?php
-            if(have_posts()){
-                while(have_posts()){
-                    the_post();?>
-
-            <h2><?php the_title(); ?></h2>
-            <?php the_content();
-                }//end while loop
-            }
-
-          ?>
-        </div>
-  
-        <div class="three columns">
-          <?php get_sidebar(); ?>
-        </div>
-        </div>
+          <div class="twelve columns">
+              <?php dynamic_sidebar('hero-image-blog'); ?>
+          </div>
       </div>
+
+<!-- About Us Text Block Section -->
+    <div class="row">
+        <div class="two-thirds column single">
+            <?php dynamic_sidebar('about-us-single'); ?>
+        </div>
+        <div class="one-third column sidebar single">
+          <!-- sidebar -->
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
 
 <?php get_footer(); ?>
