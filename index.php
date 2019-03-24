1c8dffcbff21dd7a642ff2dd007247313fdac1b4
Rachel McCart <rlmccart@ufl.edu>
2019-03-24 09:22:52 -0500

    update divs

--- index.php
+++ index.php
@@ -25,24 +25,6 @@
         }// this ends the if statement
     ?>
       </div>
-    </div>
-    <div class="row">
-    <?php
-        if(have_posts()){
-            while(have_posts()){
-                the_post();?>
-                <div class="one-half column posts">
-                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                    <?php the_post_thumbnail('thumb'); ?>
-                    <?php the_excerpt(); ?>
-                   <p><?php echo "Published: " . get_the_date(); ?></p>
-                   <p><?php echo "Article written by: " . get_the_author(); ?></p>
-                </div>
-<?php        }// this ends the while loop
-        }// this ends the if statement
-    ?>
-      </div>
-    </div>
 
     <div class="row">
     <?php
@@ -60,7 +42,23 @@
         }// this ends the if statement
     ?>
       </div>
-    </div>
+
+    <div class="row">
+    <?php
+        if(have_posts()){
+            while(have_posts()){
+                the_post();?>
+                <div class="one-half column posts">
+                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                    <?php the_post_thumbnail('thumb'); ?>
+                    <?php the_excerpt(); ?>
+                   <p><?php echo "Published: " . get_the_date(); ?></p>
+                   <p><?php echo "Article written by: " . get_the_author(); ?></p>
+                </div>
+<?php        }// this ends the while loop
+        }// this ends the if statement
+    ?>
+      </div>
 </div>
 
 <?php get_footer(); ?>
