32186059cca554cd1d40a1a2dac32a7bfdc6c2be
Rachel McCart <rlmccart@ufl.edu>
2019-03-24 10:32:14 -0500

    update columns

--- index.php
+++ index.php
@@ -8,19 +8,19 @@
         </div>
     </div>
 
-<div class="container">
+<div class="container-posts">
     <div class="row">
     <?php
         if(have_posts()){
             while(have_posts()){
                 the_post();?>
-          <!--      <div class="one-half column posts"> -->
+                <div class="one-half column posts">
                     <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                     <?php the_post_thumbnail('thumb'); ?>
                     <?php the_excerpt(); ?>
                    <p><?php echo "Published: " . get_the_date(); ?></p>
                    <p><?php echo "Article written by: " . get_the_author(); ?></p>
-            <!--    </div> -->
+                 </div>
 <?php        }// this ends the while loop
         }// this ends the if statement
     ?>
@@ -31,13 +31,13 @@
         if(have_posts()){
             while(have_posts()){
                 the_post();?>
-          <!--      <div class="one-half column posts"> -->
+                <div class="one-half column posts">
                     <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                     <?php the_post_thumbnail('thumb'); ?>
                     <?php the_excerpt(); ?>
                    <p><?php echo "Published: " . get_the_date(); ?></p>
                    <p><?php echo "Article written by: " . get_the_author(); ?></p>
-          <!--      </div> -->
+                 </div>
 <?php        }// this ends the while loop
         }// this ends the if statement
     ?>
